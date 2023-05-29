b614087fbd4713b38193992ddf26c481b165c296
alexandrubuncea <alexandrubuncea@gmail.com>
2023-05-29 11:42:13 +0000

    Deploying to gh-pages from @ alexandrubuncea/focus-design-system@4e317482928c283d1f9133ce20a2d698a0ee1c58 🚀

---
+++ README.md
@@ -0,0 +1,3 @@
+# focus-design-system
+
+This is a work in progress project where I build my personal design system that I use for learning and growing my design system skills in order to understand the engineering side of it too.
---
+++ src/stories/Banner.stories.ts
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Banner } from "../components/Banner";
+import {
+  CheckCircleIcon,
+  MegaphoneIcon,
+  InformationCircleIcon,
+  XCircleIcon,
+} from "@heroicons/react/24/solid";
+
+const meta: Meta<typeof Banner> = {
+  title: "Components/Banner",
+  component: Banner,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: {
+        type: "select",
+        options: ["positive", "info", "warning", "danger", "announcement"],
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Banner>;
+
+export const Positive: Story = {
+  args: {
+    variant: "positive",
+    content: "This is a positive banner",
+  },
+};
+
+export const Info: Story = {
+  args: {
+    variant: "info",
+    content: "This is an info banner",
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    variant: "warning",
+    content: "This is a warning banner",
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    variant: "danger",
+    content: "This is a danger banner",
+  },
+};
+
+export const Announcement: Story = {
+  args: {
+    variant: "announcement",
+    content: "This is an announcement banner",
+  },
+};
