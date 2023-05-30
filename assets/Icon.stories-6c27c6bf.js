a97efa99e17dabf7ea38edd7637eeae920041a88
alexandrubuncea <alexandrubuncea@gmail.com>
2023-05-30 19:30:20 +0000

    Deploying to gh-pages from @ alexandrubuncea/focus-design-system@7b879e9d5e56c1741f7278b3e9e72d7842e600c7 🚀

---
+++ assets/Icon.stories-6c27c6bf.js
@@ -0,0 +1,12 @@
+import{I as i,H as p}from"./Icon-80b642ed.js";import"./jsx-runtime-a888423b.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const C={title:"Components/Icon",component:i,tags:["autodocs"],argTypes:{icon:{control:{type:"select",options:Object.keys(p)}},size:{control:{type:"select",options:["20","24"]}}}},e={args:{icon:"CheckCircleIcon",size:"24"}},o={args:{icon:"CheckCircleIcon",size:"20"}};var r,s,c;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    icon: "CheckCircleIcon",
+    size: "24"
+  }
+}`,...(c=(s=e.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};var n,t,a;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    icon: "CheckCircleIcon",
+    size: "20"
+  }
+}`,...(a=(t=o.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const g=["Default","Small"];export{e as Default,o as Small,g as __namedExportsOrder,C as default};
+//# sourceMappingURL=Icon.stories-6c27c6bf.js.map
