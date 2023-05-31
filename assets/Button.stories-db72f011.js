f491a2a23da5e90ff0a9a252f75bd6fa20020990
alexandrubuncea <alexandrubuncea@gmail.com>
2023-05-31 16:51:32 +0000

    Deploying to gh-pages from @ alexandrubuncea/focus-design-system@db12e7c078154b0c6014d930cfd9853350e51539 🚀

---
+++ assets/Button.stories-db72f011.js
@@ -0,0 +1,30 @@
+import{B as h}from"./Button-2cd993a3.js";import{H as e}from"./Icon-619a3c3a.js";import"./jsx-runtime-91a467a5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";/* empty css               */import"./Loader-725daab4.js";const x={title:"Components/Button",component:h,tags:["autodocs"],argTypes:{variant:{control:{type:"select",options:["primary","secondary","ghost","danger"]}},isDisabled:{control:{type:"boolean"}},isLoading:{control:{type:"boolean"}},LeftIcon:{control:{type:"select",options:Object.keys(e)}},RightIcon:{control:{type:"select",options:Object.keys(e)}},fullWidth:{control:{type:"boolean"}}}},o={args:{variant:"primary",label:"Button",RightIcon:"ArrowRightIcon",LeftIcon:"InboxArrowDownIcon"}},r={args:{variant:"secondary",label:"Button",RightIcon:"ArrowRightIcon",LeftIcon:"InboxArrowDownIcon"}},n={args:{variant:"ghost",label:"Button",RightIcon:"ArrowRightIcon",LeftIcon:"InboxArrowDownIcon"}},t={args:{variant:"danger",label:"Button",RightIcon:"ArrowRightIcon",LeftIcon:"InboxArrowDownIcon"}};var a,c,s;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    variant: "primary",
+    label: "Button",
+    RightIcon: "ArrowRightIcon",
+    LeftIcon: "InboxArrowDownIcon"
+  }
+}`,...(s=(c=o.parameters)==null?void 0:c.docs)==null?void 0:s.source}}};var i,I,g;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary",
+    label: "Button",
+    RightIcon: "ArrowRightIcon",
+    LeftIcon: "InboxArrowDownIcon"
+  }
+}`,...(g=(I=r.parameters)==null?void 0:I.docs)==null?void 0:g.source}}};var p,l,m;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    variant: "ghost",
+    label: "Button",
+    RightIcon: "ArrowRightIcon",
+    LeftIcon: "InboxArrowDownIcon"
+  }
+}`,...(m=(l=n.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var d,u,w;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    variant: "danger",
+    label: "Button",
+    RightIcon: "ArrowRightIcon",
+    LeftIcon: "InboxArrowDownIcon"
+  }
+}`,...(w=(u=t.parameters)==null?void 0:u.docs)==null?void 0:w.source}}};const L=["Primary","Secondary","Ghost","Danger"];export{t as Danger,n as Ghost,o as Primary,r as Secondary,L as __namedExportsOrder,x as default};
+//# sourceMappingURL=Button.stories-db72f011.js.map
---
+++ assets/Introduction-730e42a2.js
@@ -0,0 +1,107 @@
+import{j as t,a as o,F as s}from"./jsx-runtime-91a467a5.js";import{M as a}from"./index-a17f7f7f.js";import{u as n}from"./index-1d576ef5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-4536a475.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./index-80ae7d84.js";import"./index-d37d4223.js";import"./index-356e4a49.js";function r(i){const e=Object.assign({h1:"h1",p:"p"},n(),i.components);return o(s,{children:[t(a,{title:"Welcome/Introduction"}),`
+`,t("style",{children:`
+    .subheading {
+      --mediumdark: '#999999';
+      font-weight: 700;
+      font-size: 13px;
+      color: #999;
+      letter-spacing: 6px;
+      line-height: 24px;
+      text-transform: uppercase;
+      margin-bottom: 12px;
+      margin-top: 40px;
+    }
+
+    .link-list {
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr 1fr;
+      row-gap: 10px;
+    }
+
+    @media (min-width: 620px) {
+      .link-list {
+        row-gap: 20px;
+        column-gap: 20px;
+        grid-template-columns: 1fr 1fr;
+      }
+    }
+
+    @media all and (-ms-high-contrast:none) {
+    .link-list {
+        display: -ms-grid;
+        -ms-grid-columns: 1fr 1fr;
+        -ms-grid-rows: 1fr 1fr;
+      }
+    }
+
+    .link-item {
+      display: block;
+      padding: 20px;
+      border: 1px solid #00000010;
+      border-radius: 5px;
+      transition: background 150ms ease-out, border 150ms ease-out, transform 150ms ease-out;
+      color: #333333;
+      display: flex;
+      align-items: flex-start;
+    }
+
+    .link-item:hover {
+      border-color: #1EA7FD50;
+      transform: translate3d(0, -3px, 0);
+      box-shadow: rgba(0, 0, 0, 0.08) 0 3px 10px 0;
+    }
+
+    .link-item:active {
+      border-color: #1EA7FD;
+      transform: translate3d(0, 0, 0);
+    }
+
+    .link-item strong {
+      font-weight: 700;
+      display: block;
+      margin-bottom: 2px;
+    }
+
+    .link-item img {
+      height: 40px;
+      width: 40px;
+      margin-right: 15px;
+      flex: none;
+    }
+
+    .link-item span,
+    .link-item p {
+      margin: 0;
+      font-size: 14px;
+      line-height: 20px;
+    }
+
+    .tip {
+      display: inline-block;
+      border-radius: 1em;
+      font-size: 11px;
+      line-height: 12px;
+      font-weight: 700;
+      background: #E7FDD8;
+      color: #66BF3C;
+      padding: 4px 12px;
+      margin-right: 10px;
+      vertical-align: top;
+    }
+
+    .tip-wrapper {
+      font-size: 13px;
+      line-height: 20px;
+      margin-top: 40px;
+      margin-bottom: 40px;
+    }
+
+    .tip-wrapper code {
+      font-size: 12px;
+      display: inline-block;
+    }
+  `}),`
+`,t(e.h1,{id:"welcome-to-focus-design-system",children:"Welcome to Focus Design System"}),`
+`,t(e.p,{children:"This is a work in progress project where I build my personal design system. I use this as an opportunity to learn and advance my design system skills and to better understand the engineering side of it. This will also power up other projects I build."})]})}function k(i={}){const{wrapper:e}=Object.assign({},n(),i.components);return e?t(e,Object.assign({},i,{children:t(r,i)})):r(i)}export{k as default};
+//# sourceMappingURL=Introduction-730e42a2.js.map
